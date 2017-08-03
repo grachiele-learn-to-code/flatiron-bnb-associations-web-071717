88bd985590322c2b70c32fda98fd0997a17a6073
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-08-03 19:05:39 -0400

    DONE

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ActiveRecord::Base
   has_many :reviews, :foreign_key => 'guest_id'
   has_many :reservations, :foreign_key => 'guest_id'
   has_many :reservations, through: :listings
-  # has_many :trips, through: :reservations
 
   def trips
-    binding.pry
-    self.reservations.where(guest.id == self.id)
+
+    Reservation.all.where("guest_id = :id", id: self.id)
   end
 
 end
